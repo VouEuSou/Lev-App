833f2264afc19e4a0793bdd742992f8d112de13f
VouEuSou <willibb01@gmail.com>
2024-09-04 12:22:39 +0000

    First commit

---
+++ lev-app/app/page2/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Página 2.</Text>
+      <a href="/">Home</a>
+    </View>
+  );
+}
